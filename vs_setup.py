725c094fea25a6de089cfb2a9698a3f7b5969f1c
superbeck <matthewjbeck@gmail.com>
2022-11-24 22:55:20 -0800

    first commit working linter and autoformat

---
+++ vs_setup.py
@@ -0,0 +1,16 @@
+""" module docstring? really?"""
+
+
+def times_three(inp: int) -> int:
+    """simple function for vs setup"""
+    return inp * 3
+
+
+def main() -> None:
+    """main function for vs setup"""
+    out = times_three(5)
+    print(out)
+
+
+if __name__ == "__main__":
+    main()
